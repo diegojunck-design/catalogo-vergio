3dbaf077a33ed026131f62788aabacfdb2124f69
diegojunck-design <diego.junck@gmail.com>
2025-08-17 19:58:48 -0300

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,5 @@
+import { defineConfig } from 'astro/config';
+export default defineConfig({
+  site: 'https://SEU_USUARIO.github.io/catalogo-vergio', // ajuste para seu repo
+  output: 'static'
+});
